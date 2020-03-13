b1606ee459da4a0f83d740275181cd59cb2fc830
Sjaan Ackermann <sjaanackermann@gmail.com>
2020-03-13 09:04:45 +0200

    Hotel.php updated

--- incl/Hotel.php
+++ incl/Hotel.php
@@ -8,6 +8,22 @@
 <body>
     <?php 
     
+    class Hotel {
+
+        public $name, $dailyrate, $wifi, $pool, $bar, $spa, $buffet;
+
+        function __construct($n0, $n1, $n2, $n3, $n4, $n5, $n6) {
+            $this->name = $n0;
+            $this->dailyrate = $n1;
+            $this->wifi = $n2;
+            $this->pool = $n3;
+            $this->bar = $n4;
+            $this->spa = $n5;
+            $this->buffet = $n6;
+        }
+        
+            
+        }
     ?>
 </body>
 </html>
